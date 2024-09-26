157e863c50dbebe325c1b32048d0dc5ca1ff4a75
Reddy11786 <vbreddy@cpp.edu>
2024-09-25 23:23:09 -0700

    Established MongoDB connection and set up server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,32 @@
+// index.js
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+
+dotenv.config();
+
+// Express application instance
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to the database before starting the server
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
